b498631b9699892d0b57d1ab657fa9b08549a627
rikakohirata <rikako.hirata88@gmail.com>
2023-05-02 12:31:27 +0900

    Blogリンクを追加

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route("blog.index")} active={route().current("blog.index")}>  {/* Blogリンクを表示 */}
+                                    Blog
+                                </NavLink>
                             </div>
                         </div>
 
