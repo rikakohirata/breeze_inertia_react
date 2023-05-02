de0cc2200ad82df34ad9771445dab50e7f56c36d
rikakohirata <rikako.hirata88@gmail.com>
2023-05-02 17:33:12 +0900

    ダミーデータ追加

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -17,7 +17,8 @@ class BlogFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->text,
         ];
     }
 }
