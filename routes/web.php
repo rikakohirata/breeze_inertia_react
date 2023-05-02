2e0665464b7411e93aad600a88768dd03ed5d151
rikakohirata <rikako.hirata88@gmail.com>
2023-05-02 12:25:53 +0900

    ルーティングblog.indexの追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // ブログ
+    Route::get('/blogs', [BlogController::class, 'index'])->name('blog.index');
 });
 
 
