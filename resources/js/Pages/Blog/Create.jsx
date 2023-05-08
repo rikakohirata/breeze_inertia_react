898daf9aa0df7c2c5b890d1d3d5393f818129afa
rikakohirata <rikako.hirata88@gmail.com>
2023-05-08 14:16:08 +0900

    新規登録フォームのバリデーションを修正

--- resources/js/Pages/Blog/Create.jsx
+++ resources/js/Pages/Blog/Create.jsx
@@ -29,9 +29,9 @@ export default function Create({ auth }) {
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                             <div className="p-6 bg-white border-b border-gray-200">
-                                <InputError errors={errors} />
                                 <div>
                                     <InputLabel htmlFor="title" value="Title" />
+
                                     <TextInput
                                         type="text"
                                         name="title"
@@ -40,9 +40,12 @@ export default function Create({ auth }) {
                                         isFocused={true}
                                         onChange={(e) => setData('title', e.target.value)}
                                     />
+                                    
+                                    <InputError message={errors.title} className="mt-2" />
                                 </div>
                                 <div>
                                     <InputLabel forInput="content" value="Content" />
+
                                     <TextInput
                                         type="text"
                                         name="content"
@@ -50,6 +53,8 @@ export default function Create({ auth }) {
                                         className="mt-1 block w-full"
                                         onChange={(e) => setData('content', e.target.value)}
                                     />
+
+                                    <InputError message={errors.content} className="mt-2" />
                                 </div>
                                 <div className="flex items-center justify-end mt-4">
                                     <PrimaryButton className="ml-4" processing={processing}>  {/* POSTリクエストの処理中はボタンを無効化 */}
