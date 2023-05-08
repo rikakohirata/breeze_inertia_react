4746ceb94e0cd80467022f06463f9199226cd44c
rikakohirata <rikako.hirata88@gmail.com>
2023-05-08 09:54:07 +0900

    新規作成ボタンを追加

--- resources/js/Pages/Blog/Index.jsx
+++ resources/js/Pages/Blog/Index.jsx
@@ -1,44 +1,51 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import Button from "@/Components/PrimaryButton";
+import Button from "@/Components/PrimaryButton";  // Buttonコンポーネント
 import { Head } from '@inertiajs/react';
-import { Link } from "@inertiajs/inertia-react";
+import { Link } from "@inertiajs/inertia-react";  // inertia-reactのLink
 
 export default function Index({ auth, blogs }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Blog</h2>}>
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Blog</h2>}
+        >
             <Head title="Blog Index" />
 
             {/* 一覧表示 */}
-            <div className="p-6 bg-white border-b border-gray-200"> 
-                <div>
-                    <Link href={route("blog.create")}>
-                        <Button type="button">新規作成</Button>
-                    </Link>
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200"> 
+                            <div>
+                                <Link href={route("blog.create")}>
+                                    <Button type="button">新規作成</Button>
+                                </Link>
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>タイトル</th>
+                                        <th>コンテンツ</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {blogs.map((blog) => {
+                                        return (
+                                            <tr key={blog.id}>
+                                                <td className="border px-4 py-2">
+                                                    {blog.title}
+                                                </td>
+                                                <td className="border px-4 py-2">
+                                                    {blog.content}
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>タイトル</th>
-                            <th>コンテンツ</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {blogs.map((blog) => {
-                            return (
-                                <tr key={blog.id}>
-                                    <td className="border px-4 py-2">
-                                        {blog.title}
-                                    </td>
-                                    <td className="border px-4 py-2">
-                                        {blog.content}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
             </div>
         </AuthenticatedLayout>
     );
