0d31cec61265e7dbd41cea9e32b4b61c9eb6583f
rikakohirata <rikako.hirata88@gmail.com>
2023-05-10 18:14:35 +0900

    ルーティングでshowメソッドを追加

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::middleware('auth')->group(function () {
                 'destroy' => 'blog.destroy',
                 'edit' => 'blog.edit',
                 'update' => 'blog.update',
+                'show' => 'blog.show',
             ]);
 });
 
